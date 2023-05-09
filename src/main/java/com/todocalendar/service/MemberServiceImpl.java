039d4c5fba6ffc5fadb798e7b98dc01076ce8a56
yujinjin211 <yujins211@naver.com>
2023-05-10 03:25:23 +0900

    TestController 삭제

--- src/main/java/com/todocalendar/service/MemberServiceImpl.java
+++ src/main/java/com/todocalendar/service/MemberServiceImpl.java
@@ -7,7 +7,9 @@ import com.todocalendar.mapper.MemberMapper;
 import com.todocalendar.model.MemberVO;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
 
+@Log4j
 @Service
 @AllArgsConstructor
 public class MemberServiceImpl implements MemberService {
@@ -17,7 +19,20 @@ public class MemberServiceImpl implements MemberService {
 	
 	@Override
 	public void insertMember(MemberVO member) {
+		log.info("insertMember.........");
 		memberMapper.insertMember(member);
+	}
+	
+	@Override
+	public MemberVO readById(String id) {
+		log.info("readById........." + id);
+		return memberMapper.readById(id);
+	}
+	
+	@Override
+	public MemberVO loginCheck(MemberVO member) {
+		log.info("loginCheck........." + member);
+		return memberMapper.loginCheck(member);
 	}
 
 }
