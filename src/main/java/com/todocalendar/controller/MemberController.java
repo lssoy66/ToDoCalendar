176991ee752d10ff747db0a81973a90eddf189fe
yujinjin211 <yujins211@naver.com>
2023-06-12 21:42:22 +0900

    23/06/12 commit

--- src/main/java/com/todocalendar/controller/MemberController.java
+++ src/main/java/com/todocalendar/controller/MemberController.java
@@ -1,5 +1,8 @@
 package com.todocalendar.controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -55,12 +59,16 @@ public class MemberController {
 	}
 	
 	//로그아웃
-	@RequestMapping("/logout")
-	public String logout(HttpServletRequest request) {
+	@RequestMapping(value = "/logout", method = RequestMethod.GET, produces = "application/text; charset = UTF-8")
+	public String logout(HttpServletRequest request) throws Exception {
 		HttpSession session = request.getSession();
 		session.invalidate();
-		log.info("로그아웃");
-		return "/pages/login";
+		log.info("logout.........");
+		
+		request.setAttribute("msg", "로그아웃 되셨습니다.");
+		request.setAttribute("url", "./login");
+		
+		return "alert";
 	}
 	
 	//회원가입 페이지 이동
@@ -92,8 +100,8 @@ public class MemberController {
 		log.info("mypage.........");
 	}
 	
-	//자동미루기 on Ajax
-	@PostMapping("/DelayAutoOn")
+	//자동미루기 Ajax
+	@PostMapping("/ChangeDelayAuto")
 	@ResponseBody
 	public ResponseEntity<MemberVO> onDelayAuto(String delay_auto, HttpSession session) {
 		log.info("delay_auto : " + delay_auto);
@@ -106,17 +114,21 @@ public class MemberController {
 		return new ResponseEntity<>(memberService.readById(id), HttpStatus.OK);
 	}
 	
-	@PostMapping("/DelayAutoOff")
-	@ResponseBody
-	public ResponseEntity<MemberVO> offDelayAuto(String delay_auto, HttpSession session) {
-		log.info("delay_auto : " + delay_auto);
+	//회원정보 수정
+	@PostMapping("/updateMember")
+	public String updateMember(Model model, HttpServletRequest request) {
 		
-		MemberVO member = (MemberVO)session.getAttribute("member");
-		String id = member.getId();
-		log.info("member id : " + id);
-		memberService.changeDelayAuto(id, delay_auto);
+		request.setAttribute("msg", "회원정보가 수정되었습니다.");
+		request.setAttribute("url", "./mypage");
 		
-		return new ResponseEntity<>(memberService.readById(id), HttpStatus.OK);
+		return "";
+	}
+	
+	//비밀번호 변경
+	@RequestMapping(value = "/updatePW", method = RequestMethod.GET)
+	public String updatePassword() {
+		log.info("update password.........");
+		return "/pages/update_pw";
 	}
 	
 }
